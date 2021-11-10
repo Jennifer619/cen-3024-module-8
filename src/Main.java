c631f45b0c9f55698b5ee6320d1a280896ca5f35
Jen <jmiller283@mail.valenciacollege.edu>
2021-11-09 20:53:30 -0500

    create main

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Main {
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int[] arr = new int[200000000];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = rand.nextInt(10) + 1;
+		}
+		long start = System.currentTimeMillis();
+		System.out.println(ParallelArraySum.sum(arr));
+		System.out.println("Single: " + (System.currentTimeMillis() - start));
+		start = System.currentTimeMillis();
+		System.out.println(ParallelArraySum.parallelSum(arr));
+		System.out.println("Parallel: " + (System.currentTimeMillis() - start));
+	}
+}
